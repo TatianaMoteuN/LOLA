961b9d70c32c34af5926baa4cb3dc2d8eddd9fad
TatianaMoteuN <mtatiana@aimsammi.org>
2023-05-17 14:17:07 +0200

    updating test file

--- test.py
+++ test.py
@@ -7,15 +7,7 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import MBartTokenizer, MBartModel, T5Tokenizer, T5ForConditionalGeneration, MT5ForConditionalGeneration
 
 
-article = """
-Albert Einstein ( 14 March 1879 – 18 April 1955) was a German-born theoretical physicist, widely acknowledged to be one of the greatest physicists of all time. 
-Einstein is best known for developing the theory of relativity, but he also made important contributions to the development of the theory of quantum mechanics. 
-Relativity and quantum mechanics are together the two pillars of modern physics. 
-His mass–energy equivalence formula E = mc2, which arises from relativity theory, has been dubbed "the world's most famous equation". 
-His work is also known for its influence on the philosophy of science.
-He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect", a pivotal step in the development of quantum theory. 
-His intellectual achievements and originality resulted in "Einstein" becoming synonymous with "genius"
-"""
+article = "UN Chief Says There Is No Military Solution in Syria"
 
 
 def get_translation_model_and_tokenizer(src_lang, dst_lang):
@@ -46,7 +38,7 @@ dst = "zh"
 model, mbart_tokenizer, model2, mt5_tokenizer = get_translation_model_and_tokenizer(src, dst)
 
 # encode the text into tensor of integers using the appropriate tokenizer
-inputs = mbart_tokenizer.encode(article, return_tensors="pt", max_length=512, truncation=True)
+inputs = mbart_tokenizer.encode(article, return_tensors="pt", max_new_tokens=512, truncation=True)
 print(inputs)
 
 # generate the translation output using beam search
